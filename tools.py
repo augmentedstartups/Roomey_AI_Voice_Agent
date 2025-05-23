a49985e8394a52aaa45bd6f3d79de80da3fba9cf
Augmented Startups <reigngt09@gmail.com>
2025-05-23 17:12:41 +0200

    working

--- tools.py
+++ tools.py
@@ -6,6 +6,7 @@ from google.genai import types
 
 set_light_values_declaration = {
     "name": "set_light_values",
+    "behavior": "NON_BLOCKING",
     "description": "Sets the brightness and color temperature of a light.",
     "parameters": {
         "type": "object",
@@ -23,6 +24,13 @@ set_light_values_declaration = {
         "required": ["brightness", "color_temp"],
     },
 }
+
+Add in a tool for getting stocks\
+    stock_price_agent = Agent(
+    name='stock_agent',
+    instructions= 'You are an agent who retrieves stock prices. If a ticker symbol is provided, fetch the current price. If only a company name is given, first perform a Google search to find the correct ticker symbol before retrieving the stock price. If the provided ticker symbol is invalid or data cannot be retrieved, inform the user that the stock price could not be found.',
+    handoff_description='This agent specializes in retrieving real-time stock prices within 2 decimal places. Given a stock ticker symbol (e.g., AAPL, GOOG, MSFT) or the stock name, use the tools and reliable data sources to provide the most up-to-date price.',
+    tools=[get_stock_price,WebSearchTool(search_context_size="low")],
 
 
 # ===== FUNCTIONS =====
@@ -45,7 +53,7 @@ def set_light_values(brightness: int, color_temp: str) -> dict[str, int | str]:
 def get_tools():
     """Returns the list of tools configured for the AI assistant."""
     return [
-        # types.Tool( google_search=types.GoogleSearch()),
-        types.Tool( function_declarations=[set_light_values_declaration]),
+        {"google_search": {}},
+        {"function_declarations": [set_light_values_declaration]},
     ]
 
